2a297aaab13b535a0aa757c8282e90b3812775a0
chrismadufor <chrismadufor.com>
2022-11-29 19:28:41 +0100

    changed the text for the Enterprise Content Management slide

--- components/carousels/heroSliderFile.js
+++ components/carousels/heroSliderFile.js
@@ -1,14 +1,14 @@
 const HeroSliderObject = [
     {
         heading: 'Techspecialist Consulting Limited',
-        subText: 'Bridging the gap between users and technology',
+        subText: 'Bridging the gap between users and technology.',
         image: '/assets/img/backup.png',
         link: 'company',
         position: 'right'
     },
     {
         heading: 'It Support',
-        subText: 'Over a decade of providing Managed IT Services to support enterprise organizations',
+        subText: 'Over a decade of providing Managed IT Services to support enterprise organizations.',
         image: '/assets/img/it-page.jpg',
         link: 'it-support',
         position: ''
@@ -22,21 +22,21 @@ const HeroSliderObject = [
     },
     {
         heading: 'Cloud Services',
-        subText: 'Helping You Unlock and Maximize the Benefits of the Cloud',
+        subText: 'Helping You Unlock and Maximize the Benefits of the Cloud.',
         image: '/assets/img/cloud-service.jpg',
         link: 'cloud-services',
         position: 'right'
     },
     {
         heading: 'Enterprise Content Management',
-        subText: 'Leverage Techspecialist DocTrack to manage physical and electronic documents while retaining their integrity and authenticity.',
+        subText: 'Leverage Techspecialist DocTrack to manage your documents without undermining file security.',
         image: '/assets/img/ecm.jpg',
         link: 'it-support#doc__management',
         position: 'right'
     },
     {
         heading: 'Neca Awards',
-        subText: 'Neca Excellence Award winner for best and most responsible company in ICT category',
+        subText: 'Neca Excellence Award winner for best and most responsible company in ICT category.',
         image: '/assets/img/neca.png',
         position: 'right'
     },
