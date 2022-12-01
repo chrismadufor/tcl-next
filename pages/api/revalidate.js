b6b69f2a8a47130deef397b504cc214f31c854cb
chrismadufor <chrismadufor.com>
2022-12-01 10:21:44 +0100

    resolve webhook issue and gallery page bug

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,12 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 const handler = async (req, res) => {
-  if(req.query.secret !== process.env.NEXT_APP_MY_SECRET_KEY) {
-    return res.status(401).json({message: `Invalid token... Req key --> ${req.query} ... env key --> ${process.env.NEXT_APP_MY_SECRET_KEY}`})
-  }
+  // console.log(req)
+  // if(req.query.secret !== process.env.NEXT_APP_MY_SECRET_KEY) {
+  //   return res.status(401).json({
+  //     message: 'Invalid token',
+  // })
+  // }
   try {
+    console.log(req)
     await res.revalidate("/blog")
-    return res.send({revalidated: true})
+    return res.send({revalidated: true, data: req})
   }catch (err) {
     return res.status(500).send("Error revalidating")
   }
